0e9d8e310df63b9783d2b18fd5962b7280ba34a1
jopesy <joonsyys@gmail.com>
2016-04-19 14:52:46 +0300

    Add form for editing posts

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
+    title = "New Post"
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -25,4 +26,20 @@ def post_new(request):
     else:
         form = PostForm()
 
-    return render(request, 'blog/post_new.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form, 'title': title})
+
+def post_edit(request, pk):
+    title = "Edit Post"
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.edited_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form': form, 'title': title})
